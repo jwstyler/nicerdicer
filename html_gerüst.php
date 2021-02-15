de308cf790fe5cce504cd19d95982426a14f99a4
Josef <71771624+jwstyler@users.noreply.github.com>
2021-02-15 19:22:14 +0100

    Add files via upload

---
+++ html_gerüst.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <title>Titel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="CSS/mainStyle.css">
+    <!-- JavaScript -->
+    <script type="text/javascript" src="JS-Skripte/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript" src="JS-Skripte/main.js"></script>
+  </head>
+  <body>
+    <!-- Sichtbarer Dokumentinhalt im body -->
+    <p>Sehen Sie sich den Quellcode dieser Seite an.
+      <kbd>(Kontextmenu: Seitenquelltext anzeigen)</kbd></p>
+  </body>
+</html>
