5b299597f93b70f09327b927b48a6dc99a2b324a
Josef <71771624+jwstyler@users.noreply.github.com>
2021-04-18 11:56:05 +0200

    Update html_gerüst.php

--- html_gerüst.php
+++ html_gerüst.php
@@ -2,7 +2,7 @@
 <html lang="de">
     <head>
         <meta charset="utf-8">
-        <title>Filmverwaltung</title>
+        <title>MainPage</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Stylesheets -->
         <link rel="stylesheet" type="text/css" href="CSS/mainStyle.css">
