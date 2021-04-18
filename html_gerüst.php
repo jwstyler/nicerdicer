f30a74560132c8c6a7982215894ffa7d5a91bfc6
Josef <71771624+jwstyler@users.noreply.github.com>
2021-04-18 11:55:50 +0200

    Update html_gerüst.php

--- html_gerüst.php
+++ html_gerüst.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html lang="de">
-  <head>
-    <meta charset="utf-8">
-    <title>Titel</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <title>Filmverwaltung</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Stylesheets -->
+        <link rel="stylesheet" type="text/css" href="CSS/mainStyle.css">
+        <!-- JavaScript -->
+        <script type="text/javascript" src="JS/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="JS/main.js"></script>
+    </head>
+    <body>
 
-    <!-- Stylesheets -->
-    <link rel="stylesheet" type="text/css" href="CSS/mainStyle.css">
-    <!-- JavaScript -->
-    <script type="text/javascript" src="JS-Skripte/jquery-3.5.1.min.js"></script>
-    <script type="text/javascript" src="JS-Skripte/main.js"></script>
-  </head>
-  <body>
-    <!-- Sichtbarer Dokumentinhalt im body -->
-    <p>Sehen Sie sich den Quellcode dieser Seite an.
-      <kbd>(Kontextmenu: Seitenquelltext anzeigen)</kbd></p>
-  </body>
+
+
+    </body>
 </html>
